1195baf6f88e3049e207992468d93116cf07d204
John-Lauritzen <92867440+John-Lauritzen@users.noreply.github.com>
2021-12-02 15:38:36 -0700

    Final day 2 solutions.

--- Day-2.py
+++ Day-2.py
@@ -1,18 +1,16 @@
-#day2data = open("Day2-Input.txt", "r")
-data = ['forward 5','down 5','forward 8','up 3','down 8','forward 2']
+day2data = open("Day2-Input.txt", "r")
+#data = ['forward 5','down 5','forward 8','up 3','down 8','forward 2']
 direction = []
 distance = []
-#for line in day2data:
-for line in data:
+for line in day2data:
+#for line in data:
     value = line.split(' ')
     tempdirection = value[0]
     tempdistance = int(value[1])
     direction.append(tempdirection)
     distance.append(tempdistance)
 
-#day2data.close()
-
-
+day2data.close()
 
 def calcmovement(directlist, distlist):
     depth = 0
@@ -26,5 +24,21 @@ def calcmovement(directlist, distlist):
             depth -= distlist[i]
     print("Horizontal is:", horizontal, 'Depth is:', depth, 'Product is:', horizontal * depth)
 
+def advcalcmovement(directlist, distlist):
+    depth = 0
+    horizontal = 0
+    aim = 0
+    for i in range(len(directlist)):
+        if directlist[i] == 'forward':
+            depth += distlist[i] * aim
+            horizontal += distlist[i]
+        elif directlist[i] == 'down':
+            aim += distlist[i]
+        else:
+            aim -= distlist[i]
+    print("Horizontal is:", horizontal, 'Depth is:', depth, 'Product is:', horizontal * depth)
+
 
 calcmovement(direction, distance)
+
+advcalcmovement(direction, distance)
